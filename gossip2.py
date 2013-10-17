d7727e90a65fd7cbc898954b19cbefd0d50dd21d
Niklas Semmler <metaswirl@gmail.com>
2013-10-18 01:05:34 +0200

    deleted unnecessary file

--- gossip2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-'''
-Title: Gossip Basic Functions
-Author: Niklas Semmler
-'''
-
-FILE_LIST_OF_NEIGHBOURS = "/etc/gossip_neighbors"
-RESULT_FILE = "/hosthome/new_home/" # TODO: file rotation
